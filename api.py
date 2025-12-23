3be991a24d3db2fa65d7e1e52096f4d361d77c13
hassan <ms09070@st.habib.edu.pk>
2025-12-23 16:11:49 +0500

    test

--- api.py
+++ api.py
@@ -26,7 +26,7 @@ async def convert_pdf(
             page_range=(page_start, page_end)
         )
 
-        markdown_output = result.document.export_to_markdown()
+        markdown_output = result.document.export_to_dict()
 
         return {
             "filename": file.filename,
